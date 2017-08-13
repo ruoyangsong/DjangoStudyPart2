e10fb766c5e95b178a4d7d2ffdb3e1a531267dc9
ruoyangsong <ruoyang@ualberta.ca>
2017-08-13 22:14:21 +0800

    create database

--- my_blog/my_blog/settings.py
+++ my_blog/my_blog/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'artical.apps.ArticalConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
